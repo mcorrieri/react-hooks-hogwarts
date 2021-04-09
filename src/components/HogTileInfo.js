18e0fb0036a2c7e7016612218e826ea02b6a17b5
Jon <jbrundagejr@gmail.com>
2021-04-09 11:52:33 -0400

    filter and sort need to be done

--- src/components/HogTileInfo.js
+++ src/components/HogTileInfo.js
@@ -3,7 +3,7 @@ function HogTileInfo({specialty, greased, weight, medal}) {
         <div>
         <ul>
             <li>Specialty: {specialty}</li>
-            <li>Greased: {greased ? "Yes" : "No"}</li>
+            <li>Greased: {greased ? "Heck Yes" : "Oh hell Naw"}</li>
             <li>Weight: {weight}</li>
             <li>Highest Medal Achieved: {medal}</li>
         </ul>
