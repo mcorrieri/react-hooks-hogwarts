140acf7dcbc1e561c81106bce0afb638f59bdc19
Michael <Mlcorrieri@gmail.com>
2021-04-09 13:50:50 -0400

    Filter almost done

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,4 +1,15 @@
-function Filter() {
-  return <h1>Filter</h1>;
+function Filter({ onCategoryChange }) {
+  function handleCategoryChange(event) {
+    onCategoryChange(event.target.value);
+  }
+  return (
+    <div className="filterWrapper">
+      <select name="filter" onChange={handleCategoryChange}>
+        <option value="All">Filter By Greased</option>
+        <option value="true">Greased</option>
+        <option value="false">Not Greased</option>
+      </select>
+    </div>
+  );
 }
 export default Filter;
--- src/components/HogTileList.js
+++ src/components/HogTileList.js
@@ -1,26 +1,38 @@
-import HogTile from "./HogTile"
+import HogTile from "./HogTile";
 import Sort from "./Sort";
 import Filter from "./Filter";
-
+import React, { useState } from "react";
 
 function HogTileList({ hogInfo }) {
-  const hogArr = hogInfo.map((hog) => {
-    return <HogTile 
-        key={hog.name}
-        name={hog.name} 
-        image={hog.image}
-        specialty={hog.specialty}
-        greased={hog.greased}
-        weight={hog.weight}
-        medal={hog["highest medal achieved"]}
-    />;
+  const [isSelectedCat, setIsSelectedCat] = useState("All");
+
+  function onCategoryChange(isSelectedCat) {
+    setIsSelectedCat(isSelectedCat);
+  }
+  const hogsToDisplay = hogInfo.filter((hog) => {
+    console.log(hog.greased);
+    if (isSelectedCat === "All") return true;
+
+    return hog.greased === isSelectedCat;
   });
 
   return (
     <div>
-      <Filter />
+      <Filter onCategoryChange={onCategoryChange} />
       <Sort />
-      <ul>{hogArr}</ul>
+      <ul>
+        {hogsToDisplay.map((hog) => (
+          <HogTile
+            key={hog.name}
+            name={hog.name}
+            image={hog.image}
+            specialty={hog.specialty}
+            greased={hog.greased}
+            weight={hog.weight}
+            medal={hog["highest medal achieved"]}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
